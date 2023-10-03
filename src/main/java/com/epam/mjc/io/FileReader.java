d59213fb15e35c4ee74e5e68fd5035be6a72385f
Sandro Suqnidze <sandrosuqnidze2@gmail.com>
2023-10-03 16:35:19 +0400

    FileReader

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,50 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
+
+        String content = "";
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            int character;
+            while ((character = fileInputStream.read()) != -1) {
+                content += (char) character;
+            }
+            System.out.print(content);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String[] lines = content.split("\n");
+        Profile profile = new Profile();
+
+        for (String line : lines) {
+            String[] keyValue = line.split(": ");
+            if (keyValue.length == 2) {
+                String key = keyValue[0];
+                String value = keyValue[1];
+
+                switch (key) {
+                    case "Name":
+                        profile.setName(value);
+                        break;
+                    case "Age":
+                        profile.setAge(Integer.parseInt(value));
+                        break;
+                    case "Email":
+                        profile.setEmail(value);
+                        break;
+                    case "Phone":
+                        profile.setPhone(Long.parseLong(value));
+                        break;
+                }
+            }
+        }
+
+
         return new Profile();
     }
 }
